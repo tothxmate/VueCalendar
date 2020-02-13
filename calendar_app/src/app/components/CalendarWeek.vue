759fede91781498e539082708214470d622fea11
John Mayer <johnxmayer@Johns-MBP.home>
2020-02-13 10:48:07 +0100

    Added select functionality

---
+++ calendar_app/src/app/components/CalendarWeek.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="calendar-week" class="container">
+        <div class="columns is-mobile">
+            <CalendarDay v-for="day in sharedState.seedData"
+                :key="day.id"
+                :day="day" />
+        </div>
+    </div>
+</template>
+<script>
+import CalendarDay from "./CalendarDay.vue";
+import { store } from "../store.js";
+export default {
+    name: "CalendarWeek",
+    data () {
+        return {
+            sharedState: store.state
+        }
+    },
+    components: {
+        CalendarDay
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+#calendar-week {
+  margin-bottom: 50px;
+  .column {
+    padding: 0 0 0 0;
+  }
+}
+
+</style>
