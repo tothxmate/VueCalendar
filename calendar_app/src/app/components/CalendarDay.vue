7c58fb513e07792fdb88d602df9d06bcdda9ce2a
John Mayer <johnxmayer@Johns-MBP.home>
2020-02-13 22:27:23 +0100

    Finished last changes on the calendar

--- calendar_app/src/app/components/CalendarDay.vue
+++ calendar_app/src/app/components/CalendarDay.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="day column">
+    <div class="day column" @click="setActiveDay(day.id)">
           <div class="day-banner has-text-white has-text-centered"> {{ day.abbvTitle }}</div>
           <div class="day-details">
             <div class="day-number">{{ day.id }}</div>
@@ -12,9 +12,15 @@
 </template>
 <script>
 import CalendarEvent from "./CalendarEvent.vue";
+import { store } from "../store.js";
 export default {
     name: "CalendarDay",
     props: ["day"],
+    methods: {
+        setActiveDay(dayId){
+            store.setActiveDay(dayId);
+        }
+    },
     components: {
         CalendarEvent
     }
--- calendar_app/src/app/store.js
+++ calendar_app/src/app/store.js
@@ -12,5 +12,43 @@ export const store = {
         this.state.seedData.map((dayObj) => {
             dayObj.id === dayId ? dayObj.active = true : dayObj.active = false;
         });
+    },
+    submitEvent(eventDetails){
+        const activeDay = this.getActiveDay();
+        activeDay.events.push({"details":eventDetails, "edit": false});
+    },
+    editEvent(dayId, eventDetails){
+        this.resetEditOfAllEvents();
+        const eventObj = this.getEventObject(dayId,eventDetails);
+        eventObj.edit = true;
+    },
+    resetEditOfAllEvents(){
+        this.state.seedData.map((dayObj) => {
+            dayObj.events.map((event) => {
+                event.edit = false;
+            });
+        });
+    },
+    updateEvent(dayId, oldEventDetails, updatedEventDetails){
+        const eventObj = this.getEventObject(dayId, oldEventDetails);
+        eventObj.details = updatedEventDetails;
+        eventObj.edit = false;
+    },
+    getEventObject(dayId, eventDetails){
+        const dayObj = this.state.seedData.find(
+            day => day.id === dayId
+        );
+        return dayObj.events.find(
+            event => event.details === eventDetails
+        );
+    },
+    deleteEvent(dayId, eventDetails){
+        const dayObj = this.state.seedData.find(
+            day => day.id === dayId
+        );
+        const eventIndexToRemove = dayObj.events.findIndex(
+            event => event.details === eventDetails
+        );
+        dayObj.events.splice(eventIndexToRemove, 1);
     }
 }
